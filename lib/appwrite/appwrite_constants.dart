d4459ffff992bd0886c2f95a5b63dc9b035beb0b
Kyle Gallego <kylebrentgallegocpe@gmail.com>
2023-07-18 05:02:17 +0800

    update appwrite constants and add folders and files for event feature

--- lib/appwrite/appwrite_constants.dart
+++ lib/appwrite/appwrite_constants.dart
@@ -1,4 +1,8 @@
 class AppwriteConstants {
   static const apiEndpoint = 'https://cloud.appwrite.io/v1';
   static const projectId = '649ff58eced0e6c84a6b';
+
+  static const databaseId = '64b5a9d93f4d66657255';
+  static const eventsCollectionId = '64b5a9ebd7b7173d16f2';
+  static const attendeesCollectionId = '64b5aa9e50d3e93e3139';
 }
